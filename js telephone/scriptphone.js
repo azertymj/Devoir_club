e43c10906e9fd756c26bc3a7a4e4013ad7fbade0
azertymj <hendrixmarvinmwouabo@gmail.com>
2023-05-15 19:53:22 +0100

    modified

--- js telephone/scriptphone.js
+++ js telephone/scriptphone.js
@@ -11,7 +11,7 @@ let telephone ={
             this.active=true
             varscreen.style.backgroundImage="url('IMG/tecno-tecno_camon_x.jpg')"
             // let touchbtn = document.addEventListener('click',btnt)
-            let btnp =document.getElementById("bt").style.visibility="visible"
+            // let btnp =document.getElementById("bt").style.visibility="visible"
 
         }
     },
@@ -30,17 +30,24 @@ let telephone ={
             let numero = prompt("Entez le numero :");
             let sub = numero.substr(1,1)
             if (sub == 7 || sub == 4 || sub == 6 || sub == 2) {
+                document.querySelector("p").style.visibility="visible"
                 let write =document.querySelector("p").textContent="Ce numéro ne peut pas être appelé"
                 document.querySelector("p").style.color="red"
+                varscreen.style.backgroundImage=""
+                varscreen.style.backgroundColor="green"
+                setTimeout(() => {
+                    this.acceuil()
+                }, 3000);
             }else{
+                document.querySelector("p").style.visibility="visible"
                 let write =document.querySelector("p").textContent="appel en cours ..."
                 varscreen.style.backgroundImage=""
                 varscreen.style.backgroundColor="green"
                 let btnp =document.getElementById("bt").style.visibility="visible"
                 let touchbtn = document.querySelector("button") 
                 touchbtn.addEventListener('click',()=>{
-                    console.log("touchbtn");
-                    this.allumer()
+                    this.acceuil()
+                    
                 })
                 // console.log(touchbtn)
             }
@@ -50,6 +57,8 @@ let telephone ={
 
     message:function () {
         if (this.active) {
+            varscreen.style.backgroundImage=""
+            varscreen.style.backgroundColor="aqua"
             let numero = prompt("Entez le numero :");
             let sub = numero.substr(1,1)
             console.log(sub)
@@ -57,10 +66,27 @@ let telephone ={
                 document.write("Ce numéro ne peut pas recevoir vos messages");
             }else{
                 let sms =prompt("Saisissez votre message :")
-                let write =document.querySelector("p").textContent="Message envoyé...";
+                document.querySelector("p").style.visibility="visible"
+                let write =document.querySelector("p")
+                write.textContent="Message envoyé...";
+                setTimeout(() => {
+                    write.textContent=`Vous avez envoyer ${sms} à ${numero}`;
+                }, 3000);
+                write.style.color="green"
+                setTimeout(() => {
+                    this.acceuil()
+                }, 5000);
+                
             }
             
         }
     },
 
+    acceuil:function () {
+        if (this.active) {  
+            varscreen.style.backgroundImage="url('IMG/tecno-tecno_camon_x.jpg')"
+            document.querySelector("p").style.visibility="hidden"
+            let btnp =document.getElementById("bt").style.visibility="hidden"
+        }
+    }
 }
